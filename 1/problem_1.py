3090c33938e7022ca2654a568108d4249626118a
RecheckCarrOil <charliemcrocker@gmail.com>
2021-04-21 23:33:59 +0100

    Working solution for Project Euler problem 1. Including parameterised unit tests

--- 1/problem_1.py
+++ 1/problem_1.py
@@ -1,15 +1,36 @@
 # This is a sample Python script.
+import math
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def is_multiple_of_3(intInput):
-    return intInput % 3 == 0 and intInput > 0
+def is_multiple_of_3(input_value):
+    return input_value % 3 == 0 and input_value > 0
+
+
+def is_multiple_of_5(input_value):
+    return input_value % 5 == 0 and input_value > 0
+
+
+def sum_of_multiples_of_3_or_5(start_num, end_num):
+    total = 0
+    counter = start_num
+    while counter < end_num:
+        if is_multiple_of_3(counter) or is_multiple_of_5(counter):
+            print(counter)
+            total += counter
+        counter += 1
+    return total
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     is_multiple_of_3(3)
+
+    a = math.sqrt(225)
+    print(str(a))
+
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- 1/test_problem_1.py
+++ 1/test_problem_1.py
@@ -3,6 +3,18 @@ import pytest
 from problem_1 import *
 
 
-@pytest.mark.parametrize("intInput, result", [(0, False), (2, False), (3, True), (9, True), (31, False)])
-def test_is_multiple_of_3(intInput, result):
-    assert is_multiple_of_3(intInput) == result
+@pytest.mark.parametrize("input_value, result",
+                         [(0, False), (2, False), (3, True), (9, True), (31, False), (3000, True)])
+def test_is_multiple_of_3(input_value, result):
+    assert is_multiple_of_3(input_value) == result
+
+
+@pytest.mark.parametrize("input_value, result",
+                         [(0, False), (2, False), (5, True), (9, False), (54, False), (555, True)])
+def test_is_multiple_of_5(input_value, result):
+    assert is_multiple_of_5(input_value) == result
+
+
+@pytest.mark.parametrize("start_num, end_num, result", [(1, 10, 23), (3, 4, 3), (1, 20, 78), (1, 1000, 233168)])
+def test_sum_multiples_in_range(start_num, end_num, result):
+    assert sum_of_multiples_of_3_or_5(start_num, end_num) == result
